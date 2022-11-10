c7e05fc10635fd7f8e3a755b865dbc86cef9f930
daeunkwak <misodaeun3@naver.com>
2022-11-10 11:38:20 +0900

    fix: MemberImage, CompanyRegisterImage 칼럼 삭제 수정

--- src/main/java/app/adoneadmin/domain/image/companyRegister/CompanyRegisterImageRepository.java
+++ src/main/java/app/adoneadmin/domain/image/companyRegister/CompanyRegisterImageRepository.java
@@ -3,9 +3,11 @@ package app.adoneadmin.domain.image.companyRegister;
 import app.adoneadmin.domain.image.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CompanyRegisterImageRepository extends JpaRepository<Image,Long> {
 
     @Query("select ci.imageId from CompanyRegisterImage ci where ci.member.memberId =:memberId")
-    Long findByMemberId(Long memberId);
+    Long findByMemberId(@Param("memberId") Long memberId);
+
 }
