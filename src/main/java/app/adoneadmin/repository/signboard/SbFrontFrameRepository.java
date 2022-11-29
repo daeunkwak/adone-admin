12699baef08c25c5d6672dc62da81f91a1a0bae3
daeunkwak <misodaeun3@naver.com>
2022-11-29 15:57:12 +0900

    fix: 빌드 에러 수정

--- src/main/java/app/adoneadmin/repository/signboard/SbFrontFrameRepository.java
+++
@@ -1,8 +0,0 @@
-package app.adoneadmin.repository.signboard;
-
-import app.adoneadmin.domain.signboard.SbFrontFrame;
-import app.adoneadmin.domain.signboard.SbFrontTruss;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SbFrontFrameRepository extends JpaRepository<SbFrontTruss, Long> {
-}
--- src/main/java/app/adoneadmin/repository/signboard/frontframe/SbFrontFrameRepository.java
+++ src/main/java/app/adoneadmin/repository/signboard/frontframe/SbFrontFrameRepository.java
@@ -3,7 +3,7 @@ package app.adoneadmin.repository.signboard.frontframe;
 import app.adoneadmin.domain.signboard.SbFrontFrame;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SbFrontFrameRepository extends JpaRepository<SbFrontFrame, Long>, SbFrontFrameRepositoryQuerydsl {
+public interface SbFrontFrameRepository extends JpaRepository<SbFrontFrame, Long> {
 
 //    @Query(value = "select gs from SbFrontFrameGalvaStan gs where gs.galva not -1")
 //    Collection<Object> findGalva();
