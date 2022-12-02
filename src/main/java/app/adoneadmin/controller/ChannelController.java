89bb5a3b2e771f89a51a030894e787bff620d603
daeunkwak <misodaeun3@naver.com>
2022-12-02 14:01:00 +0900

    feat: 전광채널 api 개발중

---
+++ src/main/java/app/adoneadmin/controller/ChannelController.java
@@ -0,0 +1,19 @@
+package app.adoneadmin.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "channels", description = "채널 단가 api")
+@RequestMapping("/api/channels")
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+public class ChannelController {
+
+
+
+
+}
